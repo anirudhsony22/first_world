5b22fac13b1d8ee593c9cfacbce73679fd0209b9
anirudhsony22 <anirudha.psoni@gmail.com>
2019-09-10 00:14:31 +0530

    CodeChef September Challenge 1

--- fibonacci.py
+++ fibonacci.py
@@ -4,10 +4,14 @@ def fib(n):
     m=0
     i=0
     while(i!=n):
-        k=m+l
-        L.append(m)
-        m=l
-        l=k
+        L.append(m%10)
+        l=m+l
+        m=l-m
         i+=1
+    while(len(L)!=1):
+        m=[]
+        for i in range(1,len(L),2):
+            m.append(L[i])
+        L=m
     return L
 print(fib(10))
