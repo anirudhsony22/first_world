1d83acca8e14baa802aebcabb7e4e91b15a781cb
anirudhsony22 <anirudha.psoni@gmail.com>
2019-09-13 01:07:13 +0530

    Hackerrank C Proficiency

---
+++ digitfrequency.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int res[10]={0},j=0;
+    char num[1000];
+    scanf("%s",&num);
+    for(int i=0;i<strlen(num);i++){
+        if(num[i]>='0' && num[i]<='9'){
+            char b=num[i];
+            //printf("%c\n",b);
+            j=atoi(&b);
+            printf("%d\n",j);
+            res[j]+=1;
+            j = 0;
+
+        }
+    }
+    return 0;
+}
